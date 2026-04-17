56fa5ce686507f86775860d01a985818f579fefa
lilenalopes354-debug <lilenalopes354@gmail.com>
2026-04-16 21:19:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# meu-portfolio-react
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/lilenalopes354-debug/meu-portfolio-react)
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+import Projetos from "./Projetos"; // Aqui estamos importando o componente criado
+
+export default function App() {
+  return (
+    <div className="portfolio">
+      <header>
+        <h1>Meu Portfólio</h1>
+      </header>
+
+      <main>
+        <section id="sobre">
+          <h2>Sobre Mim</h2>
+          {/* Coloque o link da foto da Lilena aqui se tiver */}
+          <img src="https://raw.githubusercontent.com/lilenalopes354-debug/Portf-lio-Lilena-UAPI/principal/foto.jpg.jpeg" alt="Foto de Lilena" className="foto" />
+          <p>Sou estudante de Sistemas para Internet (UAPI) e foco em soluções digitais.</p>
+        </section>
+
+        {/* Aqui chamamos o componente de projetos - Cumpre o requisito de componentização */}
+        <Projetos />
+      </main>
+    </div>
+  );
+}
